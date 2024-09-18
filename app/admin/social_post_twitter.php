6797f751a77fa01e69cb4762a83f8913fb32b291
uwamba <uwambadodo@gmail.com>
2024-09-18 19:48:56 +0200

    social connection

--- app/admin/social_post_twitter.php
+++ app/admin/social_post_twitter.php
@@ -20,6 +20,14 @@ $request->setHeader(array(
 $mediaUploadUrl = 'https://upload.twitter.com/1.1/media/upload.json';
 $request2 = new HTTP_Request2($mediaUploadUrl);
 $request2->setMethod(HTTP_Request2::METHOD_POST);
+$request2->setConfig(array(
+    'follow_redirects' => TRUE
+  ));
+  $request2->setHeader(array(
+    'Content-Type' => 'application/json',
+    'Authorization' => 'OAuth oauth_consumer_key="QTibt0PJtKFHi8gk19X9MSXE6",oauth_token="1832316580367101952-0AdQIS4EGcsEVMurYiTxbzZqM0n8O7",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1726676931",oauth_nonce="ZGGDNWzydoG",oauth_version="1.0",oauth_signature="bCaL8td%2BgGnn4VU00tZGj2WjH4A%3D"',
+    'Cookie' => 'guest_id=v1%3A172667623597701603'
+  ));
 
 $mediaFile = 'post_img/post2.jpeg';
 $request2->addPostParameter([
