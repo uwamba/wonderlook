0c590d489c56f2651ecdf61b47f38981b6df4202
uwamba <uwambadodo@gmail.com>
2024-09-18 19:47:01 +0200

    social connection

--- app/admin/social_post_twitter.php
+++ app/admin/social_post_twitter.php
@@ -5,6 +5,7 @@ use HTTP_Request2;
 
 $request = new HTTP_Request2();
 $request->setUrl('https://api.twitter.com/2/tweets');
+
 $request->setMethod(HTTP_Request2::METHOD_POST);
 $request->setConfig(array(
   'follow_redirects' => TRUE
@@ -14,14 +15,20 @@ $request->setHeader(array(
   'Authorization' => 'OAuth oauth_consumer_key="QTibt0PJtKFHi8gk19X9MSXE6",oauth_token="1832316580367101952-0AdQIS4EGcsEVMurYiTxbzZqM0n8O7",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1726676931",oauth_nonce="ZGGDNWzydoG",oauth_version="1.0",oauth_signature="bCaL8td%2BgGnn4VU00tZGj2WjH4A%3D"',
   'Cookie' => 'guest_id=v1%3A172667623597701603'
 ));
+
+
+$mediaUploadUrl = 'https://upload.twitter.com/1.1/media/upload.json';
+$request2 = new HTTP_Request2($mediaUploadUrl);
+$request2->setMethod(HTTP_Request2::METHOD_POST);
+
 $mediaFile = 'post_img/post2.jpeg';
-$request->addPostParameter([
+$request2->addPostParameter([
     'media' => new CURLFile($mediaFile),
 ]);
 $mediaId ="";
 try {
     // Send media upload request
-    $response = $request->send();
+    $response = $request2->send();
     $responseData = json_decode($response->getBody(), true);
 
     if (isset($responseData['media_id_string'])) {
