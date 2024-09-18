e0c52475509d34c05fc85a6464753195fd3c13f3
uwamba <uwambadodo@gmail.com>
2024-09-18 19:56:01 +0200

    social connection

--- app/admin/social_post_twitter.php
+++ app/admin/social_post_twitter.php
@@ -3,18 +3,7 @@ require '../vendor/autoload.php';
 use HTTP_Request2;
 
 
-$request = new HTTP_Request2();
-$request->setUrl('https://api.twitter.com/2/tweets');
 
-$request->setMethod(HTTP_Request2::METHOD_POST);
-$request->setConfig(array(
-  'follow_redirects' => TRUE
-));
-$request->setHeader(array(
-  'Content-Type' => 'application/json',
-  'Authorization' => 'OAuth oauth_consumer_key="QTibt0PJtKFHi8gk19X9MSXE6",oauth_token="1832316580367101952-0AdQIS4EGcsEVMurYiTxbzZqM0n8O7",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1726676931",oauth_nonce="ZGGDNWzydoG",oauth_version="1.0",oauth_signature="bCaL8td%2BgGnn4VU00tZGj2WjH4A%3D"',
-  'Cookie' => 'guest_id=v1%3A172667623597701603'
-));
 
 
 $mediaUploadUrl = 'https://upload.twitter.com/1.1/media/upload.json';
@@ -48,7 +37,18 @@ try {
     echo 'Error: ' . $e->getMessage();
     exit;
 }
+$request = new HTTP_Request2();
+$request->setUrl('https://api.twitter.com/2/tweets');
 
+$request->setMethod(HTTP_Request2::METHOD_POST);
+$request->setConfig(array(
+  'follow_redirects' => TRUE
+));
+$request->setHeader(array(
+  'Content-Type' => 'application/json',
+  'Authorization' => 'OAuth oauth_consumer_key="QTibt0PJtKFHi8gk19X9MSXE6",oauth_token="1832316580367101952-0AdQIS4EGcsEVMurYiTxbzZqM0n8O7",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1726676931",oauth_nonce="ZGGDNWzydoG",oauth_version="1.0",oauth_signature="bCaL8td%2BgGnn4VU00tZGj2WjH4A%3D"',
+  'Cookie' => 'guest_id=v1%3A172667623597701603'
+));
 $request->setBody('{"text": "Hello everyone!","media_ids" => $mediaId}');
 try {
   $response = $request->send();
